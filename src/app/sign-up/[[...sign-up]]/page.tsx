18009959ea7c76ff79ce98d6755862082ad91760
thecmdrunner <thecmdrunner@proton.me>
2023-11-24 16:16:12 +0530

    fix layout

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -8,31 +8,30 @@ export default function Page() {
     <>
       <div className="container relative grid min-h-screen flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-          <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-            <div
-              style={{
-                backgroundImage:
-                  "url('https://img.freepik.com/free-vector/gradient-network-connection-background_23-2148871843.jpg')",
-              }}
-              className="absolute inset-0 bg-zinc-900 bg-cover bg-center bg-no-repeat"
-            />
-            <div className="relative z-20 flex items-center text-lg font-medium">
-              <LucideTerminal className="mr-2 h-6 w-6" /> Dev Expo
-            </div>
-            <div className="relative z-20 mt-auto">
-              <blockquote className="space-y-2">
-                <p className="text-lg">
-                  &ldquo;Lorem ipsum dolor sit amet consectetur adipisicing
-                  elit. Nulla, odio tempore incidunt facilis quis fuga rerum!
-                  Impedit ipsa tenetur quaerat recusandae sapiente.&rdquo;
-                </p>
-                <footer className="text-sm">Lorem Ipsum</footer>
-              </blockquote>
-            </div>
+          <div
+            style={{
+              backgroundImage:
+                "url('https://img.freepik.com/free-vector/gradient-network-connection-background_23-2148871843.jpg')",
+            }}
+            className="absolute inset-0 bg-zinc-900 bg-cover bg-center bg-no-repeat"
+          />
+          <div className="relative z-20 flex items-center text-lg font-medium">
+            <LucideTerminal className="mr-2 h-6 w-6" /> Dev Expo
           </div>
-          <div className="lg:p-8">
-            <div className="mx-auto flex w-full flex-col items-center justify-start space-y-6 sm:w-80">
-              {/* <div className="flex flex-col space-y-2 text-center">
+          <div className="relative z-20 mt-auto">
+            <blockquote className="space-y-2">
+              <p className="text-lg">
+                &ldquo;Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Nulla, odio tempore incidunt facilis quis fuga rerum! Impedit
+                ipsa tenetur quaerat recusandae sapiente.&rdquo;
+              </p>
+              <footer className="text-sm">Lorem Ipsum</footer>
+            </blockquote>
+          </div>
+        </div>
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col items-center justify-start space-y-6 sm:w-80">
+            {/* <div className="flex flex-col space-y-2 text-center">
           <h1 className="text-2xl font-semibold tracking-tight">
             Create an account
           </h1>
@@ -40,25 +39,24 @@ export default function Page() {
             Enter your email below to create your account
           </p>
         </div> */}
-              <UserAuthForm />
-              <p className="px-8 text-center text-sm text-muted-foreground">
-                By clicking continue, you agree to our{' '}
-                <Link
-                  href={URLs.termsOfService}
-                  className="underline underline-offset-4 hover:text-primary"
-                >
-                  Terms of Service
-                </Link>{' '}
-                and{' '}
-                <Link
-                  href={URLs.privacyPolicy}
-                  className="underline underline-offset-4 hover:text-primary"
-                >
-                  Privacy Policy
-                </Link>
-                .
-              </p>
-            </div>
+            <UserAuthForm />
+            <p className="px-8 text-center text-sm text-muted-foreground">
+              By clicking continue, you agree to our{' '}
+              <Link
+                href={URLs.termsOfService}
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Terms of Service
+              </Link>{' '}
+              and{' '}
+              <Link
+                href={URLs.privacyPolicy}
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Privacy Policy
+              </Link>
+              .
+            </p>
           </div>
         </div>
       </div>
