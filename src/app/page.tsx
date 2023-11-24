f1744662ccd798cbed7d78fea2a27ea497790a77
thecmdrunner <thecmdrunner@proton.me>
2023-11-25 01:55:17 +0530

    minor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import { UserButton } from '@clerk/nextjs';
+import { URLs } from '@/lib/constants';
+import Link from 'next/link';
 
 export default function Page() {
   return (
     <div>
       <h1>Page</h1>
 
-      <UserButton />
+      <Link href={URLs.signIn}>Sign in</Link>
     </div>
   );
 }
