b52e1372c691df5985fa4b37f063b0c704988a97
thecmdrunner <thecmdrunner@proton.me>
2023-11-25 01:58:12 +0530

    rm clerk urls

--- src/env.js
+++ src/env.js
@@ -23,19 +23,6 @@ export const env = createEnv({
 
     NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
     NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
-
-    NEXT_PUBLIC_CLERK_SIGN_IN_URL: z
-      .string()
-      .refine((url) => url.startsWith('/')),
-    NEXT_PUBLIC_CLERK_SIGN_UP_URL: z
-      .string()
-      .refine((url) => url.startsWith('/')),
-    NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: z
-      .string()
-      .refine((url) => url.startsWith('/')),
-    NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: z
-      .string()
-      .refine((url) => url.startsWith('/')),
   },
 
   /**
@@ -43,19 +30,12 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
-    NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
-    NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL:
-      process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL,
-    NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL:
-      process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
