75950519abd3fa8158d68d2067ed141f692b90ef
thecmdrunner <thecmdrunner@proton.me>
2023-11-25 01:21:30 +0530

    add supabase

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,35 @@
-import { authMiddleware } from '@clerk/nextjs';
-import { URLs } from './lib/constants';
+import { createClient } from '@/utils/supabase/middleware';
+import { NextResponse, type NextRequest } from 'next/server';
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({
-  publicRoutes: [URLs.home, URLs.termsOfService, URLs.privacyPolicy],
-});
+// export default authMiddleware({
+//   publicRoutes: [URLs.home, URLs.termsOfService, URLs.privacyPolicy],
+// });
+
+export async function middleware(request: NextRequest) {
+  try {
+    // This `try/catch` block is only here for the interactive tutorial.
+    // Feel free to remove once you have Supabase connected.
+    const { supabase, response } = createClient(request);
+
+    // Refresh session if expired - required for Server Components
+    // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
+    await supabase.auth.getSession();
+
+    return response;
+  } catch (e) {
+    // If you are here, a Supabase client could not be created!
+    // This is likely because you have not set up environment variables.
+    // Check out http://localhost:3000 for Next Steps.
+    return NextResponse.next({
+      request: {
+        headers: request.headers,
+      },
+    });
+  }
+}
 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
