be2c754b5558066bf4349b77a059ffde78a3c031
Kishan Mandora <kishanmandora7@gmail.com>
2023-11-25 02:51:11 +0530

    styles: add shadcn with slate theme and dark/light mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { UserButton } from '@clerk/nextjs';
+import { ModeToggle } from "@/components/ui/toggle";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Page() {
   return (
     <div>
-      <h1>Page</h1>
-
+      <h1 className="bg-background text-foreground">Page</h1>
+      <ModeToggle />
       <UserButton />
     </div>
   );
