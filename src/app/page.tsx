b47b245e174d1f318d6e3ded49a965c1de4ab9bd
Kishan Mandora <kishanmandora7@gmail.com>
2023-11-25 12:17:41 +0530

    feat: add radio btns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from '@/components/ui/navbar';
+import RadioBtns from '@/components/ui/radio-btns';
 
 export default function Page() {
   return (
     <div>
       <Navbar />
+      <RadioBtns />
     </div>
   );
 }
