8883992a00261a5670cbbe3044d5705e65c56a45
thecmdrunner <thecmdrunner@proton.me>
2023-11-25 23:21:33 +0530

    light default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
           <ThemeProvider
             enableSystem
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             disableTransitionOnChange
           >
             {children}
