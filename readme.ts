186e60f4634727941469ccf2e4d97bb29935d2cf
thecmdrunner <thecmdrunner@proton.me>
2023-11-26 18:06:03 +0530

    fix

--- readme.ts
+++
@@ -1,5 +0,0 @@
-const url = `https://raw.githubusercontent.com/thecmdrunner/remotion-render-alternatives/main/README.md`;
-
-const readme = await fetch(url).then((res) => res.text());
-
-console.log(readme);
