f94858045a44e786c8c08c67cff2f0f409856db3
thecmdrunner <thecmdrunner@proton.me>
2023-11-26 16:02:53 +0530

    auth fix

--- src/hooks/user/profile.ts
+++ src/hooks/user/profile.ts
@@ -1,9 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import { getOrCreateUserProfile } from './actions';
+import { useAuth } from './auth';
 
 export const useUserProfile = () => {
+  const { session, isLoaded } = useAuth();
+
   return useQuery({
     queryKey: ['user_profile'],
     queryFn: () => getOrCreateUserProfile(),
+
+    // Only run this query when the user is logged in
+    enabled: isLoaded && Boolean(session?.user.id),
   });
 };
