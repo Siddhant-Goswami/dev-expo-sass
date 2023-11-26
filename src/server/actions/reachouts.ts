a486208c1321012395daa613082f8af401e5c954
Kishan Mandora <kishanmandora7@gmail.com>
2023-11-27 00:00:34 +0530

    fix: linter and prettier

--- src/server/actions/reachouts.ts
+++ src/server/actions/reachouts.ts
@@ -27,15 +27,21 @@ const createRecruiterReachout = async ({
     throw new Error('Unauthenticated');
   }
 
-  const recruiterId = session.user.id;
-  await db.insert(recruiterReachouts).values({
-    recruiterId,
-    devId,
-    workType: workType as 'freelance' | 'full-time',
-    quotePrice,
-    message,
-    timestamp: new Date(),
-  });
+  try {
+    const recruiterId = session.user.id;
+    await db.insert(recruiterReachouts).values({
+      recruiterId,
+      devId,
+      workType: workType as 'freelance' | 'full-time',
+      quotePrice,
+      message,
+      timestamp: new Date(),
+    });
+
+    return { success: true, message: 'Reachout sent' };
+  } catch (error) {
+    return { success: false, message: 'Something went wrong' };
+  }
 };
 
 // const getIncomingReachouts = async ({ userId }: { userId: number }) => {
