f0d321875f598f45a76820f95fcab1c6d6860076
Kishan Mandora <kishanmandora7@gmail.com>
2023-11-27 12:00:42 +0530

    fix: add logo as svg component

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,14 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 'use client';
 
-import brandLogo from '@/../public/images/100xTalent.svg';
 import { Button } from '@/components/ui/button';
+import Logo from '@/components/ui/logo';
 import ProjectUploadModal from '@/components/ui/project-upload-modal';
 import SignUpModal from '@/components/ui/sign-up-modal';
 import { ThemeToggle } from '@/components/ui/toggle';
 import { useAuth } from '@/hooks/user/auth';
 import { useUserProfile } from '@/hooks/user/profile';
-import Image from 'next/image';
 import Link from 'next/link';
 import UserAuthButton from '../UserAuthButton';
 
@@ -25,7 +24,7 @@ const NavBar = () => {
       <div className="flex w-full items-center justify-between space-x-4">
         <div className="flex items-center">
           <Link href="/" className="text-2xl font-semibold">
-            <Image src={brandLogo} width={150} height={90} alt="100xTalent" />
+            <Logo />
           </Link>
         </div>
         <div className="flex h-10 items-center gap-6">
