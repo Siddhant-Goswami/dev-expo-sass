14e30d2ac257142bae0afbbd8a23f1475c655b26
Siddhant Goswami <goswami.siddh@gmail.com>
2023-11-27 20:45:59 +0530

    Prettier format

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,10 +2,10 @@
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://77fe8e29501736c1d2d1e7adf4205ef4@o4506297707986944.ingest.sentry.io/4506297709559808",
+  dsn: 'https://77fe8e29501736c1d2d1e7adf4205ef4@o4506297707986944.ingest.sentry.io/4506297709559808',
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -3,10 +3,10 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://77fe8e29501736c1d2d1e7adf4205ef4@o4506297707986944.ingest.sentry.io/4506297709559808",
+  dsn: 'https://77fe8e29501736c1d2d1e7adf4205ef4@o4506297707986944.ingest.sentry.io/4506297709559808',
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
