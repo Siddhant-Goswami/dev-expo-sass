51279fc7b7105138509bc3fd6d1ee911436eaec9
rishabhgurbani <rishabh.gurbani23@gmail.com>
2023-11-28 23:10:19 +0530

    markdown support

--- src/app/feed/[...id]/page.tsx
+++ src/app/feed/[...id]/page.tsx
@@ -1,10 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import Footer from '@/components/ui/footer';
 import NavBar from '@/components/ui/navbar';
-import Markdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-import rehypeRaw from 'rehype-raw'
-import rehypeSanitize from 'rehype-sanitize';
 
 
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
@@ -23,6 +19,7 @@ import { getProjectById } from '@/server/actions/projects';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { notFound } from 'next/navigation';
+import MarkdownComponent from '@/components/mark-down';
 
 type PageProps = {
   params: { id: string[] };
@@ -148,21 +145,7 @@ async function Page({ params }: PageProps) {
           ))}
 
           <div className="mt-8">
-            <Markdown 
-            components={
-              {
-                h1: ({node, ...props}) => <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl" {...props} />,
-                h2: ({node, ...props}) => <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0" {...props} />,
-                h3: ({node, ...props}) => <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight" {...props} />,
-                h4: ({node, ...props}) => <h4 className="scroll-m-20 text-xl font-semibold tracking-tight" {...props} />,
-                code: ({node, ...props}) => <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold" {...props} />,
-                p: ({node, ...props}) => <p className="leading-7 [&:not(:first-child)]:mt-6" {...props} />,
-                ul: ({node, ...props}) => <ul className="my-6 ml-6 list-disc [&>li]:mt-2" {...props} />,
-              }
-            }
-            remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw, rehypeSanitize]}>
-              {description}
-            </Markdown>
+            <MarkdownComponent content={description}/>
           </div>
 
           {!isSameUser && (
