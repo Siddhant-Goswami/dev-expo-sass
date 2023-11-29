2143ed79235f5f4f9b94cc96eb731f46cf8b206d
Pranav <thecmdrunner@proton.me>
2023-11-30 02:33:45 +0530

    analytics, events (#54)
    * analytics, events
    
    * prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,13 @@ import { Analytics } from '@vercel/analytics/react';
 import { ThemeProvider } from '@/components/theme/ThemeProvider';
 import { Toaster } from '@/components/ui/toaster';
 import '@/styles/globals.css';
+import NextTopLoader from 'nextjs-toploader';
 
+import { env } from '@/env';
 import { TRPCReactProvider } from '@/trpc/react';
 import localFont from 'next/font/local';
 import { headers } from 'next/headers';
+import Script from 'next/script';
 
 const NeueMont = localFont({
   src: [
@@ -48,6 +51,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <NextTopLoader
+        showSpinner={false}
+        color="#39AD44"
+        shadow={false}
+        easing="ease-in-out"
+      />
+
+      <Script id="tawkto" strategy="afterInteractive">
+        {`
+         var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+         (function () {
+             var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+             s1.async = true;
+             s1.src = '${env.NEXT_PUBLIC_TAWK_TO_SRC_URL}';
+             s1.charset = 'UTF-8';
+             s1.setAttribute('crossorigin', '*');
+             s0.parentNode.insertBefore(s1, s0);
+         })();
+        `}
+      </Script>
+
       <body className={NeueMont.className}>
         <TRPCReactProvider headers={headers()}>
           <ThemeProvider
