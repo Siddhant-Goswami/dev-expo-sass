f864c13176a380e677ac08dc1f9adad47a193f85
Rishabh Gurbani <rishabh.gurbani23@gmail.com>
2023-11-29 16:39:41 +0530

    Feat/video recorder (#49)
    * added video-recorder
    
    * prettier
    
    * add err log
    
    * nicer icons
    
    * nicer logo
    
    * prettier
    
    ---------
    
    Co-authored-by: thecmdrunner <thecmdrunner@proton.me>

--- src/components/ui/toggle.tsx
+++ src/components/ui/toggle.tsx
@@ -1,39 +1,46 @@
 'use client';
 
-import { Moon, Sun } from 'lucide-react';
+import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
+
 import { useTheme } from 'next-themes';
 
 import { Button } from '@/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme();
+  const { setTheme, theme } = useTheme();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme('light')}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    // <DropdownMenu>
+    //   <DropdownMenuTrigger asChild>
+
+    //   </DropdownMenuTrigger>
+    //   <DropdownMenuContent align="end">
+    //     <DropdownMenuItem onClick={() => setTheme('light')}>
+    //       Light
+    //     </DropdownMenuItem>
+    //     <DropdownMenuItem onClick={() => setTheme('dark')}>
+    //       Dark
+    //     </DropdownMenuItem>
+    //     <DropdownMenuItem onClick={() => setTheme('system')}>
+    //       System
+    //     </DropdownMenuItem>
+    //   </DropdownMenuContent>
+    // </DropdownMenu>
+    <Button
+      type="button"
+      onClick={() => {
+        if (theme === 'dark') {
+          setTheme('light');
+        } else if (theme === 'light') {
+          setTheme('dark');
+        }
+      }}
+      variant="outline"
+      size="icon"
+    >
+      <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
   );
 }
