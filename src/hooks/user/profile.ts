62ef803d02118699de0d623f95be194b36339668
thecmdrunner <38887390+thecmdrunner@users.noreply.github.com>
2023-12-01 17:29:21 +0530

    minor

--- src/hooks/user/profile.ts
+++ src/hooks/user/profile.ts
@@ -1,14 +1,2 @@
-import { useQuery } from '@tanstack/react-query';
-import { getOrCreateUserProfile } from './actions';
-import { useAuth } from './auth';
-
-export const useUserProfile = () => {
-  const { session, isLoaded } = useAuth();
-  return useQuery({
-    queryKey: ['user_profile'],
-    queryFn: () => getOrCreateUserProfile(),
-
-    // Only run this query when the user is logged in
-    enabled: isLoaded && Boolean(session?.user.id),
-  });
-};
+// import { useQuery } from '@tanstack/react-query';
+// import { useAuth } from './auth';
