ae7f20693ec8b3b88c1781010a3669c1b969cceb
thecmdrunner <38887390+thecmdrunner@users.noreply.github.com>
2023-12-01 17:32:24 +0530

    lint

--- src/components/magicui/shimmer-button.tsx
+++ src/components/magicui/shimmer-button.tsx
@@ -51,7 +51,7 @@ const ShimmerButton = React.forwardRef<HTMLButtonElement, ShimmerButtonProps>(
           {/* spark */}
           <div className="absolute inset-0 h-[100cqh] animate-slide [aspect-ratio:1] [border-radius:0] [mask:none] ">
             {/* spark before */}
-            <div className="animate-shimmer-spin absolute inset-[-100%] w-auto rotate-0 [background:conic-gradient(from_calc(270deg-(var(--spread)*0.5)),transparent_0,hsl(0_0%_100%/1)_var(--spread),transparent_var(--spread))] [translate:0_0]" />
+            <div className="absolute inset-[-100%] w-auto rotate-0 animate-shimmer-spin [background:conic-gradient(from_calc(270deg-(var(--spread)*0.5)),transparent_0,hsl(0_0%_100%/1)_var(--spread),transparent_var(--spread))] [translate:0_0]" />
           </div>
         </div>
 
