22259c7b51107e7ccac6a9c1c239833859e1ae31
Pranav <38887390+thecmdrunner@users.noreply.github.com>
2023-12-05 17:09:04 +0530

    Feat/sort feed likes (#84)
    * added fead sorting by likes
    
    * pretty
    
    * zod
    
    * ts
    
    ---------
    
    Co-authored-by: rishabhgurbani <rishabh.gurbani23@gmail.com>

--- src/server/actions/projects.ts
+++ src/server/actions/projects.ts
@@ -2,7 +2,7 @@
 import { URLs } from '@/lib/constants';
 import { projectFormSchema } from '@/lib/validations/project';
 import { createServerActionClient } from '@supabase/auth-helpers-nextjs';
-import { and, desc, eq, sql } from 'drizzle-orm';
+import { and, desc, eq, inArray, sql } from 'drizzle-orm';
 import { revalidatePath } from 'next/cache';
 import { cookies } from 'next/headers';
 import { z } from 'zod';
@@ -20,17 +20,67 @@ import {
   type UserProfileSelect,
 } from '../db/schema';
 
-// TODO: filter categories
+const getProjectsValidationSchema = z.object({
+  limit: z.number().optional(),
+  offset: z.number().optional(),
+});
 
+export const getAllProjectsSortedByLikes = async (
+  _props: z.infer<typeof getProjectsValidationSchema>,
+) => {
+  const { limit, offset } = getProjectsValidationSchema.parse(_props);
+  const likesCount = sql<number>`cast(COUNT(${likes}.id) as int)`;
+
+  const newProjects = await db
+    .select({
+      project: projects,
+      likesCount,
+      userProfile: userProfiles,
+    })
+    .from(projects)
+    .leftJoin(likes, eq(likes.projectId, projects.id))
+    .orderBy(desc(likesCount))
+    .limit(limit ? limit : 8)
+    .leftJoin(userProfiles, eq(userProfiles.id, projects.userId))
+    .groupBy(projects.id, userProfiles.id);
+
+  const projectIds = newProjects.map((p) => p.project.id);
+  const mediaPromise = db
+    .select()
+    .from(projectMedia)
+    .where(inArray(projectMedia.projectId, projectIds));
+  const tagsPromise = db.query.projectTags.findMany({
+    where: inArray(projectTags.projectId, projectIds),
+    with: {
+      tag: true,
+    },
+  });
+
+  const [media, tags] = await Promise.all([mediaPromise, tagsPromise]);
+
+  const result = newProjects.map((data) => {
+    const { project, likesCount, userProfile } = data;
+    return {
+      project,
+      likesCount,
+      user: userProfile!,
+      tags: tags
+        .filter((pt) => pt.projectId === project.id)
+        .filter((pt) => pt.tag.name !== 'ignore_this_tag')
+        .map((pt) => pt.tag),
+      media: media.filter((m) => m.projectId === project.id),
+    };
+  });
+  return result;
+};
+
+// TODO: filter categories
 // get all projects ordered by date
 // for home feed
-export const getAllProjects = async ({
-  limit,
-  offset,
-}: {
-  limit?: number;
-  offset?: number;
-}) => {
+export const getAllProjects = async (
+  _props: z.infer<typeof getProjectsValidationSchema>,
+) => {
+  const { limit, offset } = getProjectsValidationSchema.parse(_props);
   const allProjects = await db.query.projects.findMany({
     where: (p, { isNotNull }) => isNotNull(p.publishedAt),
     with: {
@@ -155,12 +205,9 @@ export const isLikedByUser = async ({ projectId }: { projectId: number }) => {
     const [result] = await db
       .select({ count: sql<number>`cast(count(${likes}) as int)` })
       .from(likes)
-      .where(
-        and(
-          eq(likes.projectId, 31),
-          eq(likes.userId, '2212f56a-2463-48e1-900b-81cdbe802e86'),
-        ),
-      );
+      .where(and(eq(likes.projectId, projectId), eq(likes.userId, userId)));
+
+    console.log('result', result);
 
     const userLikeRecord = z.coerce
       .number()
