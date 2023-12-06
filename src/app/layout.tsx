c441b8ce48b61fb86b2942d72b32b83814104c11
thecmdrunner <38887390+thecmdrunner@users.noreply.github.com>
2023-12-06 19:11:57 +0530

    add MICROSOFT_CLARITY

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,6 +78,15 @@ export default function RootLayout({
           </ThemeProvider>
         </TRPCReactProvider>
       </body>
+      <Script id="tawkto" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "${env.NEXT_PUBLIC_MICROSOFT_CLARITY_CODE}");
+        `}
+      </Script>
     </html>
   );
 }
