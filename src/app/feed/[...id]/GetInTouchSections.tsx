45b0a8d205ca143b71bb764404f804c4b8a31949
rishabhgurbani <rishabh.gurbani23@gmail.com>
2023-12-07 16:46:42 +0530

    major UI changes

--- src/app/feed/[...id]/GetInTouchSections.tsx
+++ src/app/feed/[...id]/GetInTouchSections.tsx
@@ -1,35 +1,28 @@
 'use client';
-import { Button } from '@/components/ui/button';
 import GetInTouchModal from '@/components/ui/get-in-touch-modal';
-import SignInModal from '@/components/ui/sign-up-modal';
 import { useAuth } from '@/hooks/user/auth';
+import Image from 'next/image';
 
 type GetInTouchButtonProps = {
   displayName: string;
   devId: string;
+  buttonVariant?: 'brand' | 'secondaryAction';
 };
 
 export const GetInTouchButton = ({
   displayName,
   devId,
+  buttonVariant,
 }: GetInTouchButtonProps) => {
-  const { userId } = useAuth();
-
   return (
     <div className="flex justify-center">
-      {userId ? (
-        <GetInTouchModal
-          username={displayName}
-          devId={devId}
-          text="Get in Touch"
-        />
-      ) : (
-        <SignInModal>
-          <Button variant="brand" className="w-fit">
-            Login To Get in Touch
-          </Button>
-        </SignInModal>
-      )}
+      <GetInTouchModal
+        username={displayName}
+        devId={devId}
+        text="Get in Touch"
+        roundedFull={true}
+        buttonVariant={buttonVariant}
+      />
     </div>
   );
 };
@@ -49,19 +42,30 @@ export const IsNotSameUserWrapper = ({
 export const GetInTouchSection = ({
   projectUserId,
   userDisplayName,
+  displayPictureUrl,
 }: {
   projectUserId: string;
   userDisplayName: string;
+  displayPictureUrl: string;
 }) => {
   return (
     <IsNotSameUserWrapper projectUserId={projectUserId}>
-      <div className="mt-6 flex w-full flex-col items-center justify-center border-t border-gray-500 py-8">
-        <h3 className="text-xl font-medium">Liked {userDisplayName}'s work?</h3>
-        <p className="mb-4 mt-2 text-sm">
-          Get in touch with {userDisplayName} to discuss your project.
-        </p>
+      <div className="relative mt-6 flex w-full flex-col items-center justify-center gap-4 overflow-clip rounded-t-xl border-x border-t border-[#1F5D2B] p-10 pt-16">
+        <div className="absolute h-1/4 w-1/4 rounded-full bg-green-600 bg-opacity-70 blur-[370.40px]"></div>
+        <div className="flex w-full flex-col items-center justify-center gap-3 md:flex-row">
+          <Image
+            className="aspect-square h-16 w-16 rounded-full"
+            width={100}
+            height={100}
+            src={displayPictureUrl}
+            alt={userDisplayName}
+          />
+          <div className="px-2 text-center text-xl font-medium md:text-2xl">
+            Hire {userDisplayName} for your next GenAI project
+          </div>
+        </div>
+        <GetInTouchButton displayName={userDisplayName} devId={projectUserId} />
       </div>
-      <GetInTouchButton displayName={userDisplayName} devId={projectUserId} />
     </IsNotSameUserWrapper>
   );
 };
