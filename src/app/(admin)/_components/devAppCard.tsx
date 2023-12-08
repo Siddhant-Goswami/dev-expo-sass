c57785793907745c79e56cc107694b59e3abdce7
Ajinkya Bodke <ajinkyabodke678@gmail.com>
2023-12-08 15:51:35 +0530

    add dev app card

---
+++ src/app/(admin)/_components/devAppCard.tsx
@@ -0,0 +1,62 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { DevApplicationSelect } from '@/server/db/schema';
+
+import { LucideGithub } from 'lucide-react';
+
+export function DevApplicationCard({
+  application,
+}: {
+  application: DevApplicationSelect;
+}) {
+  return (
+    <Card key={application.id} className=" grid max-w-md gap-4">
+      <CardHeader className="space-y-1">
+        <CardTitle className="text-2xl">{application.displayName}</CardTitle>
+
+        <CardDescription>{application.bio}</CardDescription>
+      </CardHeader>
+      <CardContent className="grid gap-4">
+        <div className="grid grid-cols-2 gap-6">
+          <Button variant="outline" className="aspect-square h-10 w-10 p-0">
+            <LucideGithub />
+          </Button>
+          <Button variant="outline" className="aspect-square h-10 w-10 p-0">
+            𝕏
+          </Button>
+        </div>
+        <div className="relative">
+          <div className="absolute inset-0 flex items-center">
+            <span className="w-full border-t" />
+          </div>
+          <div className="relative flex justify-center text-xs uppercase">
+            <span className="bg-background px-2 text-muted-foreground">
+              Or continue with
+            </span>
+          </div>
+        </div>
+        <div className="grid gap-2">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" type="email" placeholder="m@example.com" />
+        </div>
+        <div className="grid gap-2">
+          <Label htmlFor="password">Password</Label>
+          <Input id="password" type="password" />
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Button className="w-full">Create account</Button>
+      </CardFooter>
+    </Card>
+  );
+}
--- src/app/(admin)/wp-admin/page.tsx
+++ src/app/(admin)/wp-admin/page.tsx
@@ -1,59 +1,24 @@
-import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { db } from '@/server/db';
-import { LucideGithub } from 'lucide-react';
+import { api } from '@/trpc/server';
+
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { DevApplicationCard } from '../_components/devAppCard';
 
 export default async function DemoCreateAccount() {
-  const applications = await db.query.devApplications.findMany();
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  const userId = session?.user.id;
+  const userRole = session?.user.role;
+  console.log({ userId, userRole });
+  const { applications } = await api.admin.getAllPendingApplications.query();
   return (
     <div>
       {applications.map((app) => (
-        <Card key={app.id} className=" grid max-w-md gap-4">
-          <CardHeader className="space-y-1">
-            <CardTitle className="text-2xl">{app.displayName}</CardTitle>
-            <CardDescription>{app.bio}</CardDescription>
-          </CardHeader>
-          <CardContent className="grid gap-4">
-            <div className="grid grid-cols-2 gap-6">
-              <Button variant="outline" className="aspect-square h-10 w-10 p-0">
-                <LucideGithub />
-              </Button>
-              <Button variant="outline" className="aspect-square h-10 w-10 p-0">
-                𝕏
-              </Button>
-            </div>
-            <div className="relative">
-              <div className="absolute inset-0 flex items-center">
-                <span className="w-full border-t" />
-              </div>
-              <div className="relative flex justify-center text-xs uppercase">
-                <span className="bg-background px-2 text-muted-foreground">
-                  Or continue with
-                </span>
-              </div>
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input id="email" type="email" placeholder="m@example.com" />
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="password">Password</Label>
-              <Input id="password" type="password" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button className="w-full">Create account</Button>
-          </CardFooter>
-        </Card>
+        <DevApplicationCard application={app} />
       ))}
     </div>
   );
