75c9bce3adc68cff3955aa8c4ac66098ca1d38be
Ajinkya Bodke <ajinkyabodke678@gmail.com>
2023-12-08 15:27:21 +0530

    add social buttons

--- src/app/(admin)/wp-admin/page.tsx
+++ src/app/(admin)/wp-admin/page.tsx
@@ -9,43 +9,52 @@ import {
 } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { db } from '@/server/db';
+import { LucideGithub } from 'lucide-react';
 
-export default function DemoCreateAccount() {
+export default async function DemoCreateAccount() {
+  const applications = await db.query.devApplications.findMany();
   return (
-    <Card>
-      <CardHeader className="space-y-1">
-        <CardTitle className="text-2xl">Create an account</CardTitle>
-        <CardDescription>
-          Enter your email below to create your account
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <div className="grid grid-cols-2 gap-6">
-          <Button variant="outline">Github</Button>
-          <Button variant="outline">Google</Button>
-        </div>
-        <div className="relative">
-          <div className="absolute inset-0 flex items-center">
-            <span className="w-full border-t" />
-          </div>
-          <div className="relative flex justify-center text-xs uppercase">
-            <span className="bg-background px-2 text-muted-foreground">
-              Or continue with
-            </span>
-          </div>
-        </div>
-        <div className="grid gap-2">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" />
-        </div>
-        <div className="grid gap-2">
-          <Label htmlFor="password">Password</Label>
-          <Input id="password" type="password" />
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button className="w-full">Create account</Button>
-      </CardFooter>
-    </Card>
+    <div>
+      {applications.map((app) => (
+        <Card key={app.id} className=" grid max-w-md gap-4">
+          <CardHeader className="space-y-1">
+            <CardTitle className="text-2xl">{app.displayName}</CardTitle>
+            <CardDescription>{app.bio}</CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div className="grid grid-cols-2 gap-6">
+              <Button variant="outline" className="aspect-square h-10 w-10 p-0">
+                <LucideGithub />
+              </Button>
+              <Button variant="outline" className="aspect-square h-10 w-10 p-0">
+                𝕏
+              </Button>
+            </div>
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  Or continue with
+                </span>
+              </div>
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" placeholder="m@example.com" />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full">Create account</Button>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
   );
 }
