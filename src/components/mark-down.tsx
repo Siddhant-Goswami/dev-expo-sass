22b9a4756d83c1c55ee74442457c86b363805c4f
thecmdrunner <38887390+thecmdrunner@users.noreply.github.com>
2023-12-16 22:38:34 +0530

    better md

--- src/components/mark-down.tsx
+++ src/components/mark-down.tsx
@@ -1,7 +1,9 @@
 import Markdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark as dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import rehypeRaw from 'rehype-raw';
 import rehypeSanitize from 'rehype-sanitize';
+import remarkGfm from 'remark-gfm';
 
 type MarkdownComponentProps = {
   content: string;
@@ -10,46 +12,70 @@ type MarkdownComponentProps = {
 function MarkdownComponent({ content }: MarkdownComponentProps) {
   return (
     <Markdown
+      // components={{
+      //   h1: ({ node, ...props }) => (
+      //     <h1
+      //       className="mb-2 scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-3xl"
+      //       {...props}
+      //     />
+      //   ),
+      //   h2: ({ node, ...props }) => (
+      //     <h2
+      //       className="mt-2 scroll-m-20 pb-2 text-xl font-semibold tracking-tight first:mt-0"
+      //       {...props}
+      //     />
+      //   ),
+      //   h3: ({ node, ...props }) => (
+      //     <h3
+      //       className="scroll-m-20 text-lg font-semibold tracking-tight"
+      //       {...props}
+      //     />
+      //   ),
+      //   h4: ({ node, ...props }) => (
+      //     <h4
+      //       className="scroll-m-20 text-xl font-semibold tracking-tight"
+      //       {...props}
+      //     />
+      //   ),
+      //   code: ({ node, ...props }) => (
+      //     <code
+      //       className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold"
+      //       {...props}
+      //     />
+      //   ),
+      //   p: ({ node, ...props }) => (
+      //     <p className="leading-7 [&:not(:first-child)]:mt-6" {...props} />
+      //   ),
+      //   ul: ({ node, ...props }) => (
+      //     <ul className="my-6 ml-6 list-disc [&>li]:mt-2" {...props} />
+      //   ),
+      //   a: ({ node, ...props }) => (
+      //     <a className="text-blue-500 hover:underline" {...props} />
+      //   ),
+      // }}
+
+      // * Fancy Code Block
       components={{
-        h1: ({ node, ...props }) => (
-          <h1
-            className="mb-2 scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-3xl"
-            {...props}
-          />
-        ),
-        h2: ({ node, ...props }) => (
-          <h2
-            className="mt-2 scroll-m-20 pb-2 text-xl font-semibold tracking-tight first:mt-0"
-            {...props}
-          />
-        ),
-        h3: ({ node, ...props }) => (
-          <h3
-            className="scroll-m-20 text-lg font-semibold tracking-tight"
-            {...props}
-          />
-        ),
-        h4: ({ node, ...props }) => (
-          <h4
-            className="scroll-m-20 text-xl font-semibold tracking-tight"
-            {...props}
-          />
-        ),
-        code: ({ node, ...props }) => (
-          <code
-            className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold"
-            {...props}
-          />
-        ),
-        p: ({ node, ...props }) => (
-          <p className="leading-7 [&:not(:first-child)]:mt-6" {...props} />
-        ),
-        ul: ({ node, ...props }) => (
-          <ul className="my-6 ml-6 list-disc [&>li]:mt-2" {...props} />
-        ),
-        a: ({ node, ...props }) => (
-          <a className="text-blue-500 hover:underline" {...props} />
-        ),
+        code(props) {
+          const { children, className, node, ...rest } = props;
+          const match = /language-(\w+)/.exec(className ?? '');
+          return match ? (
+            // @ts-expect-error - Maybe Wrong types
+            <SyntaxHighlighter
+              {...rest}
+              wrapLines={true}
+              wrapLongLines={true}
+              PreTag="div"
+              children={String(children).replace(/\n$/, '')}
+              language={match[1]}
+              style={dark}
+            />
+          ) : (
+            <code {...rest} className={className}>
+              {children}
+            </code>
+          );
+        },
       }}
       remarkPlugins={[remarkGfm]}
       rehypePlugins={[rehypeRaw, rehypeSanitize]}
